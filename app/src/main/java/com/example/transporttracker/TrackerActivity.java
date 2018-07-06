c39bdcec7f045fc0251e7774fc948e4c026cbfd4
aswathibaskaran <aswathi1102@gmail.com>
2018-07-06 18:58:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/transporttracker/TrackerActivity.java
@@ -0,0 +1,65 @@
+package com.example.transporttracker;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+import static com.example.transporttracker.LoginActivity.MY_CONSTANT_NAME;
+
+public class TrackerActivity extends Activity {
+
+    private static final int PERMISSIONS_REQUEST = 1;
+static final String names = MY_CONSTANT_NAME;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracker);
+        // Check GPS is enabled
+        System.out.println(MY_CONSTANT_NAME);
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(this, "Please enable location services", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Check location permission is granted - if it is, start
+        // the service, otherwise request the permission
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            startTrackerService();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST);
+        }
+    }
+
+    private void startTrackerService() {
+        startService(new Intent(this, TrackerService.class));
+        System.out.println("running" +" " + MY_CONSTANT_NAME);
+        System.out.println(MY_CONSTANT_NAME);
+        finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[]
+            grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST && grantResults.length == 1
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // Start the service when the permission is granted
+            startTrackerService();
+        } else {
+            finish();
+        }
+    }
+}
